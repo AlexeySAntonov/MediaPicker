7352392c8058c7579782842d4c19bf96724b3be4
Alexey Antonov <alex.antonov150@gmail.com>
2021-05-10 21:28:11 +0300

    MediaPickerView: CameraX preview.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,52 +1,60 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("kotlin-android-extensions")
-    id("kotlin-kapt")
+  id("com.android.application")
+  id("kotlin-android")
+  id("kotlin-android-extensions")
+  id("kotlin-kapt")
 }
 
 android {
-    compileSdkVersion(29)
-    buildToolsVersion("29.0.0")
-    defaultConfig {
-        applicationId = "com.aleksejantonov.mediapicker"
-        minSdkVersion(21)
-        targetSdkVersion(29)
-        versionCode = 1
-        versionName = "1.0"
+  compileSdkVersion(29)
+  buildToolsVersion("29.0.0")
+  defaultConfig {
+    applicationId = "com.aleksejantonov.mediapicker"
+    minSdkVersion(21)
+    targetSdkVersion(29)
+    versionCode = 1
+    versionName = "1.0"
+  }
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
     }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
 }
 
 object Versions {
-    const val kotlin = "1.4.32"
-    const val adapterDelegatesVersion = "4.3.0"
-    const val glideVersion = "4.11.0"
+  const val kotlin = "1.4.32"
+  const val adapterDelegatesVersion = "4.3.0"
+  const val glideVersion = "4.11.0"
+  const val camerax_version = "1.1.0-alpha04"
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
-    implementation("androidx.fragment:fragment-ktx:1.3.3")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("com.google.android.material:material:1.3.0")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+  implementation("androidx.fragment:fragment-ktx:1.3.3")
+  implementation("androidx.appcompat:appcompat:1.2.0")
+  implementation("androidx.core:core-ktx:1.3.2")
+  implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+  implementation("com.google.android.material:material:1.3.0")
 
-    implementation("com.hannesdorfmann:adapterdelegates4:${Versions.adapterDelegatesVersion}")
+  implementation("com.hannesdorfmann:adapterdelegates4:${Versions.adapterDelegatesVersion}")
 
-    implementation("com.github.bumptech.glide:glide:${Versions.glideVersion}")
-    kapt("com.github.bumptech.glide:compiler:${Versions.glideVersion}")
+  implementation("com.github.bumptech.glide:glide:${Versions.glideVersion}")
+  kapt("com.github.bumptech.glide:compiler:${Versions.glideVersion}")
 
-    implementation("com.jakewharton.timber:timber:4.7.1")
+  implementation("com.jakewharton.timber:timber:4.7.1")
+
+  // CameraX core library using camera2 implementation
+  implementation("androidx.camera:camera-camera2:${Versions.camerax_version}")
+  // CameraX Lifecycle Library
+  implementation("androidx.camera:camera-lifecycle:${Versions.camerax_version}")
+  // CameraX View class
+  implementation("androidx.camera:camera-view:1.0.0-alpha24")
 }
--- app/src/main/java/com/aleksejantonov/mediapicker/picker/adapter/delegate/CameraCaptureDelegate.kt
+++ app/src/main/java/com/aleksejantonov/mediapicker/picker/adapter/delegate/CameraCaptureDelegate.kt
@@ -4,32 +4,86 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
 import com.aleksejantonov.mediapicker.picker.adapter.delegate.items.CameraCaptureItem
 import com.aleksejantonov.mediapicker.R
 import com.aleksejantonov.mediapicker.base.ui.DiffListItem
+import com.google.common.util.concurrent.ListenableFuture
 import com.hannesdorfmann.adapterdelegates4.AbsListItemAdapterDelegate
+import kotlinx.android.synthetic.main.item_camera.view.*
+import timber.log.Timber
 
 class CameraCaptureDelegate(
-    private val listener: () -> Unit
+  private val listener: () -> Unit
 ) : AbsListItemAdapterDelegate<CameraCaptureItem, DiffListItem, CameraCaptureDelegate.ViewHolder>() {
 
-    override fun isForViewType(item: DiffListItem, items: MutableList<DiffListItem>, position: Int) = item is CameraCaptureItem
+  override fun isForViewType(item: DiffListItem, items: MutableList<DiffListItem>, position: Int) = item is CameraCaptureItem
 
-    override fun onCreateViewHolder(parent: ViewGroup): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_camera, parent, false)
-        return ViewHolder(view)
+  override fun onCreateViewHolder(parent: ViewGroup): ViewHolder {
+    val view = LayoutInflater.from(parent.context).inflate(R.layout.item_camera, parent, false)
+    return ViewHolder(view)
+  }
+
+  override fun onBindViewHolder(item: CameraCaptureItem, viewHolder: ViewHolder, payloads: MutableList<Any>) {
+    viewHolder.bind(item)
+  }
+
+  override fun onViewRecycled(holder: RecyclerView.ViewHolder) {
+    (holder as ViewHolder).releaseCamera()
+  }
+
+  inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>? = null
+    // Used to bind the lifecycle of cameras to the lifecycle owner
+    private var cameraProvider: ProcessCameraProvider? = null
+
+    fun bind(item: CameraCaptureItem) {
+      with(itemView) {
+        startCameraPreview()
+        setOnClickListener { listener.invoke() }
+      }
     }
 
-    override fun onBindViewHolder(item: CameraCaptureItem, viewHolder: ViewHolder, payloads: MutableList<Any>) {
-        viewHolder.bind(item)
-    }
+    private fun startCameraPreview() {
+      cameraProviderFuture = ProcessCameraProvider.getInstance(itemView.context)
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+      cameraProviderFuture?.addListener({
+        cameraProvider = cameraProviderFuture?.get()
 
-        fun bind(item: CameraCaptureItem) {
-            with(itemView) {
-                setOnClickListener { listener.invoke() }
-            }
+        // Preview
+        val preview = Preview.Builder()
+          .build()
+          .also {
+            it.setSurfaceProvider(itemView.viewFinder.surfaceProvider)
+          }
+
+        // Select back camera as a default
+        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+        try {
+          // Unbind use cases before rebinding
+          cameraProvider?.unbindAll()
+
+          // Bind use cases to camera
+          (itemView.context as? LifecycleOwner)?.let { cameraProvider?.bindToLifecycle(it, cameraSelector, preview) }
+
+        } catch (e: Exception) {
+          Timber.e("Use case binding failed with exception: $e")
         }
+
+      }, ContextCompat.getMainExecutor(itemView.context))
     }
+
+    fun releaseCamera() {
+      cameraProviderFuture?.cancel(true)
+      cameraProviderFuture = null
+      cameraProvider?.unbindAll()
+      cameraProvider = null
+    }
+  }
 }
